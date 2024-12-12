da9239d9627cc2a1edbe8f16d0491679ea73cf30
Yutthaphichai <yutthaphichaikk@gmail.com>
2024-12-12 21:59:59 +0700

    install bootstrap

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container mt-5">
+    <h1 class="text-primary">Hello World</h1>
+    <button class="btn btn-success">Click Me</button>
   </div>
 </template>
